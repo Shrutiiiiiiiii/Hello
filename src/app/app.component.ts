2075ce3b84a51778e8ba11dd62169395ad91d962
Shruti Dhoot <shrutidhoot2001@gmail.com>
2024-11-17 12:42:27 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FirstCompComponent } from './first-comp/first-comp.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FirstCompComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
